0100b99f7545df1f455b829711130696a629af46
bobberto <chemitgmo@gmail.com>
2025-05-08 12:55:36 +0300

    final

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+import csv
+import os
+from pathlib import Path
 from jpeg_compressor import compress_image
 from jpeg_decompressor import decompress_image
 
-for i in range(1, 101, 5):
-    compress_image("data/Lenna.png", "data/Lenna.raw", quality=i)
-    # display_compressed_image("data/Tree.raw")
-    decompress_image("data/Lenna.raw", f"decompressed/Lenna {i}.png")
+def write_table(filename, new_data):
+    file_exists = Path(filename).exists()
 
-# compress_image("data/Lenna.png", "data/Lenna.raw", quality=1)
+    with open(filename, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["Коэффициент качества сжатия", "Размер сжатого файла"])
+        writer.writerow(new_data)
+
+
+
+
+files = os.listdir("data")
+
+for file in files:
+    os.makedirs(file[:-4], exist_ok=True)
+    current_folder = file[:-4]
+
+    for i in range(0, 101, 5):
+        if i == 0:
+            quality = 1
+        else:
+            quality = i
+
+        current_raw_file = f"{current_folder}/{file[:-4]} {quality}.raw"
+        compress_image(f"data/{file}", current_raw_file, quality=quality)
+
+        if i in [0, 20, 40, 60, 80, 100]:
+            decompress_image(current_raw_file, f"{current_folder}/{file[:-4]} {i}.png")
+
+        write_table(f"{current_folder}/{file[:-4]}.csv", [i, os.path.getsize(current_raw_file)])
+        os.remove(current_raw_file)
